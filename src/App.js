385dfc88cd1890f1c650a79a8809c3fb8042bb0d
Mani Jafari <manielli@gmail.com>
2019-03-02 20:16:23 -0800

    Installed firebase, set up some config in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCw67qdJftGPW6A3tK8wg2xT8BT_NsniZY',
+            authDomain: 'auth-react-native-70921.firebaseapp.com',
+            databaseURL: 'https://auth-react-native-70921.firebaseio.com',
+            projectId: 'auth-react-native-70921',
+            storageBucket: 'auth-react-native-70921.appspot.com',
+            messagingSenderId: '707904836855'
+        })
+    }
     render() {
         return (
             <View>
