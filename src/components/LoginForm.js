32a94c162b4057aa5e8eba091861d68166e1ac6b
Mani Jafari <manielli@gmail.com>
2019-03-02 21:10:53 -0800

    Added on TextInput component field to LoginForm component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+    state = { text: '' };
+
     render() {
         return (
             <Card>
-                <CardSection />
-                <CardSection />
+                <CardSection>
+                    <TextInput 
+                        value={this.state.text}
+                        onChangeText={text => this.setState({ text: text})}
+                        style={{ height: 20, width: 100 }} 
+                    />
+                </CardSection>
+                
+                <CardSection>
+
+                </CardSection>
+                
                 <CardSection>
                     <Button>
                         Log In
