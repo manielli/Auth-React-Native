e8eba03253fed9e0795edaf5274a402af73b5d27
Mani Jafari <manielli@gmail.com>
2019-03-04 14:21:01 -0800

    Added loggedIn state to the App.js component, showing a spinner if the onAuthStateChanged is not determined yet

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import Constants from './constants';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: Constants.apiKey,
@@ -14,13 +16,38 @@ class App extends Component {
             projectId: Constants.projectId,
             storageBucket: Constants.storageBucket,
             messagingSenderId: Constants.messagingSenderId
-        })
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
     }
+
+    renderContent() {
+        if (this.state.loggedIn) {
+            return (
+                <Button>
+                    Log Out
+                </Button>
+            );
+        } else {
+            return (
+                <Spinner size='small' />
+            )
+        }
+
+        return <LoginForm />
+    }
+    
     render() {
         return (
             <View>
                 <Header headerText='Authentication' />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
